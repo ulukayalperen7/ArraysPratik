cf983b369542ad7d552cbbe475a800e833d6a14b
Alperen Ulukaya <alperenulukaya07@gmail.com>
2024-08-06 14:05:48 +0300

    display game table. Only game over with return

--- Game.java
+++ Game.java
@@ -7,7 +7,7 @@ public class Game {
     public static void main(String[] args) {
         System.out.println("Mayın Tarlasına Hoşgeldiniz !");
         MineSweeper ms = new MineSweeper(10, 10);
-        ms.arrayCreator();
+        ms.displayGame(ms.arrayCreator());
     }
 }
 
@@ -23,7 +23,7 @@ class MineSweeper {
         this.column = column;
     }
 
-    public void arrayCreator() {
+    public String[][] arrayCreator() {
 
         String[][] str = new String[row][column];
 
@@ -46,9 +46,50 @@ class MineSweeper {
             str[randRow][randCol] = "* ";
         }
 
-        for (String[] strings : str) {
+        /*  for (String[] strings : str) {
             System.out.println(Arrays.toString(strings));
+        } */
+        return str;
+    }
+
+    public void displayGame(String[][] str) {
+
+        String[][] newStr = new String[str.length][str[0].length];
+
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 0; j < str[0].length; j++) {
+                newStr[i][j] = "- ";
+            }
         }
+
+        int chooseRow;
+        int chooseCol;
+        int counter = 0;
+        do {
+
+            for (String[] strings : newStr) {
+                System.out.println(Arrays.toString(strings));
+            }
+            System.out.print("Satır giriniz: ");
+            chooseRow = scanner.nextInt();
+            System.out.print("Sütun giriniz: ");
+            chooseCol = scanner.nextInt();
+            System.out.println("===========================");
+            if (str[chooseRow][chooseCol].equals("* ")) {
+                System.out.println("Game Over!");
+                return;
+            }
+
+            for (int i = chooseRow - 1; i < chooseRow + 2; i++) {
+                for (int j = chooseCol - 1; j < chooseCol + 2; j++) {
+                    if (str[i][j].equals("* ") && i >= 0 && j >= 0 && j <= column - 1 && i <= row - 1) {
+                        counter++;
+                    }
+                }
+            }
+            newStr[chooseRow][chooseCol] = counter + "";
+
+        } while (!str[chooseRow][chooseCol].equals("* "));
 
     }
 
